303020d1c2120d8e3b644779f23d3e08004b8fcd
mbh038 <mike038@btinternet.com>
2015-11-17 05:58:58 +0000

    Ass1

---
+++ Web/Assignments/Ass1_HelloLearners.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 05:26:04 2015
+
+@author: Mike
+"""
+def helloLearners(hail):
+    print hail
+    
+helloLearners("Another hello!")
+print "Hello again!"
